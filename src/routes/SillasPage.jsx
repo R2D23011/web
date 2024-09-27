5fe0a0de0021d04e64319f3c4f93164c222b75ea
R2D23011 <carlitosaac16@gmail.com>
2024-09-27 15:28:56 -0400

    fix: min height

--- src/routes/SillasPage.jsx
+++ src/routes/SillasPage.jsx
@@ -2,7 +2,7 @@ import { SillaCatalog } from '../components/catalogo/SillaCatalog/SillaCatalog'
 
 export const SillasPage = () => {
   return (
-    <div className='d-flex flex-column justify-content-between' style={{height:'100%'}}>
+    <div className='d-flex flex-column justify-content-between' style={{height:'100%', minHeight:'600px'}}>
         <main>
           <SillaCatalog></SillaCatalog>
         </main>
