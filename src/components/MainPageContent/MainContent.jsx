0541b1ae3ff7bedd9403614755ee993f7ff0816a
R2D23011 <carlitosaac16@gmail.com>
2024-09-28 23:07:36 -0400

    fix: quite el formulario de contacto en mainpage

--- src/components/MainPageContent/MainContent.jsx
+++ src/components/MainPageContent/MainContent.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
 import './MainContent.css'
-import Map from '../Map/maps'
-import ContactForm from '../Contacto/contacto'
+import ContactForm from '../Contacto/contacto.jsx'
 
 export const MainContent = () => {
   return (
@@ -20,9 +19,15 @@ export const MainContent = () => {
         </div>
         <div className="section section4">
             <h2>Contáctanos</h2>
-            <div className='sectioncontact'>
-                <Map />
-                <ContactForm />
+            <div style={{position: 'relative', paddingBottom: '56.25%', height: '0', overflow: 'hidden'}}>
+                <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3153.019112482166!2d144.9630579153169!3d-37.8141079797517!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6ad642af0f11fd81%3A0xf577d9f0b1d0a1e!2sFederation%20Square!5e0!3m2!1sen!2sau!4v1611819441234!5m2!1sen!2sau" 
+                    width="600" 
+                    height="600" 
+                    style={{border:"0", position: 'absolute', top: '0', left: '0', width: '100%', height: '70dvh'}}
+                    allowfullscreen="" 
+                    loading="lazy">
+                </iframe>
             </div>
         </div>
         <div className="container text-center" id='galeria'>
